07886e875e115d365678648478347f31fe1591f7
mj13002600 <endoftheworld801@gmail.com>
2026-09-24 18:19:40 +0900

    Add files via upload

---
+++ spring_kadai_todo/src/main/java/com/example/springkadaitodo/controller/ToDoController.java
@@ -0,0 +1,25 @@
+package com.example.springkadaitodo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.springkadaitodo.entity.ToDo;
+import com.example.springkadaitodo.service.ToDoService;
+
+@Controller
+public class ToDoController {
+
+    @Autowired
+    private ToDoService toDoService;
+
+    @GetMapping("/todo")
+    public String getTodoList(Model model) {
+        List<ToDo> todoList = toDoService.getAllToDos();
+        model.addAttribute("todoList", todoList);
+        return "todoView";
+    }
+}
